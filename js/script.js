57a619bccbeba15eab38743590fadc3c2cf67123
Edu <eduardogalli89@gmail.com>
2024-02-05 08:28:26 -0300

    Segunda Preentrega

--- js/script.js
+++ js/script.js
@@ -8,50 +8,89 @@ let nombreUsuario= prompt("Ingresa tu nombre");
 
 bienvenidaUsuario(nombreUsuario)
 
-let productosDisponibles1 = "Arena";
-let productosDisponibles2 = "Cemento";
-let productosDisponibles3 = "Piedra";
-let respuestaUsuario = prompt("¿Qué producto estás buscando? (Escribe el número correspondiente):\n" +
-                                "1. " + productosDisponibles1 + "\n" +
-                                "2. " + productosDisponibles2 + "\n" +
-                                "3. " + productosDisponibles3 + "\n" +
-                                "Presiona x para finalizar");
+const productos = [
+    {id:1, nombre:"Arena por metro cuadrado ", precio: 5000},
+    {id:2, nombre:"Cemento 50 kg ", precio: 6000},
+    {id:3, nombre:"Piedras por metro cuadrado ", precio: 9000},
+]
 
-
-if(respuestaUsuario !== "x") {
-     opcionSeleccionada = respuestaUsuario;
+                               
+class Carrito {
+constructor() {
+                this.productos = [];
+                this.totalPagar = 0;
+            }
                                 
-    if (opcionSeleccionada >= 1 && opcionSeleccionada <= 3) {
-        alert("Perfecto, en estos momentos disponemos de ese producto");
-    } else {
-    alert("Opción no válida. Por favor, selecciona una opción válida.");
-    }
-}                           
+agregarProducto(id) {
+    
+    let producto = productos.find(prod => prod.id === id);
+    console.log(producto);
 
-while(respuestaUsuario>= 1 && respuestaUsuario<= 3){
-    switch(respuestaUsuario){
-        case "1": alert("El precio del metro cuadrado de Arena es de $5000")
-        break
-        case "2": alert("El precio de la bolsa de 50kg de cemento es de $6000")
-        break
-        case "3": alert("El precio del metro cuadrado de piedra es de $9000")
-        break
+    if (producto) {
+        this.productos.push(producto);
+        console.log("Agregaste el Producto #" + id + " al Carrito!");
+    } else {
+        console.log("No se encontró el Producto con #" + id + "!");
     }
-    if(respuestaUsuario== 1){
-        let calculoCompra=prompt("Ingrese la cantidad a comprar");
-        calculoCompra=calculoCompra*5000;
-        alert("El total de su compra es de $"+calculoCompra)
-    }else{
-    if(respuestaUsuario== 2){
-        let calculoCompra=prompt("Ingrese la cantidad a comprar");
-        calculoCompra=calculoCompra*6000;
-        alert("El total de su compra es de $"+calculoCompra) 
-    }
-    else {
-        let calculoCompra=prompt("Ingrese la cantidad a comprar");
-        calculoCompra=calculoCompra*9000;
-        alert("El total de su compra es de $"+calculoCompra)
-    }
-}    
-    respuestaUsuario = prompt("Presiona x para finalizar");
 }
+
+                                
+listarCarrito() {
+    let salida = "";
+    this.productos.forEach(item => {
+            salida += item.id + " - " + item.nombre + " - $" + item.precio + "\n";
+            })
+                                    
+            return salida;
+        }
+                                
+calcularTotalProductos() {
+                          return this.productos.length;
+                        }
+                             
+calcularTotalPagar() {
+                let total = 0;
+                this.productos.forEach(item => {
+                                           total += item.precio;
+                                        });
+                                
+                                        return total;
+                                        
+                                        
+    }
+}
+function listarProductos() {
+    let salida = "";
+
+    productos.forEach(item => {
+        salida += item.id + " - " + item.nombre + " - $" + item.precio + "\n";
+    })
+
+    return salida;
+}
+
+const carrito = new Carrito();
+let opcionSeleccionada = 10;
+
+while (opcionSeleccionada != 0) {
+    opcionSeleccionada = parseInt(prompt("Seleccione el producto a agregar al Carrito: (0 para Salir)\n\n" + listarProductos()));
+
+    if (opcionSeleccionada == 0) {
+        break;
+    }
+
+    carrito.agregarProducto(opcionSeleccionada);
+}
+
+let productosCarrito = "Detalle:\n" + carrito.listarCarrito();
+let salidaSubTotal = "Subtotal: $" + carrito.calcularTotalPagar();
+let montoFinal = "Total: $" + Math.round(carrito.calcularTotalPagar());
+alert(productosCarrito + "\n" + salidaSubTotal + "\n" + montoFinal);
+
+let producto = productos.find(item => item.id === 3);
+console.log(producto.nombre + " (" + producto.precio + " Precio)");
+
+const o = productos.find(elemento =>{
+    return elemento.id === 3;
+})  
+console.log(o)
